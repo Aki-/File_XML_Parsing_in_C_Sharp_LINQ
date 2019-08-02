b2b926cf11f297196f348a93c9d69dbcfe274e3a
Aki- <akshaydalvi24@gmail.com>
2019-08-02 20:10:52 +0530

    Add project files.

---
+++ XML_Parsing_C_Sharp_LINQ/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XML_Parsing_C_Sharp_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("./inputDoc/books.xml");
+            #region Fetch All the Books 
+            var books = from r in document.Descendants("book")
+                        select new
+                        {
+                            Author = r.Element("author").Value,
+                            Title = r.Element("title").Value,
+                            Genere = r.Element("genre").Value,
+                            Price = r.Element("price").Value,
+                            PublishDate = r.Element("publish_date").Value,
+                            Description = r.Element("description").Value,
+
+                        };
+
+            foreach (var r in books)
+            {
+                Console.WriteLine("Title:- " + r.Title + "\nPublish Date:- " + r.PublishDate + "\nAuthor:-" + r.Author + "\n\n");
+
+            }
+
+            Console.ReadKey(true);
+            #endregion 
+
+
+            /*
+            
+            #region Fetching a particular  Book            
+            var selectedBook = from r in document.Descendants("book").Where
+                                   (r=>(string)r.Attribute("id")=="bk102")
+                        select new
+                        {
+                            Author = r.Element("author").Value,
+                            Title = r.Element("title").Value,
+                            Genere = r.Element("genre").Value,
+                            Price = r.Element("price").Value,
+                            PublishDate = r.Element("publish_date").Value,
+                            Description = r.Element("description").Value,
+ 
+                        };
+ 
+            foreach (var r in selectedBook)
+            {
+                Console.WriteLine(r.PublishDate + r.Title + r.Author);
+            }
+            Console.ReadKey(true);
+            #endregion 
+ 
+          
+ 
+            #region Fetching a particular  Book
+ 
+          
+            var selectedBookAttribute = (from r in document.Descendants("book").Where
+                                        (r => (string)r.Attribute("id") == "bk102")
+                                        select r.Element("author").Attribute("id").Value).FirstOrDefault();
+            
+            Console.WriteLine(selectedBookAttribute);         
+                        
+            Console.ReadKey(true);                                  
+ 
+            #endregion
+ 
+ 
+            #region Fetching all Authors 
+ 
+           
+            var allauthors = from r in document.Descendants("book")
+                                         select r.Element("author").Value; 
+            foreach(var r in allauthors)
+            {
+                Console.WriteLine(r.ToString());
+            }
+           
+ 
+            Console.ReadKey(true);
+ 
+            #endregion
+ 
+
+             */
+
+
+
+        }
+    }
+}
---
+++ XML_Parsing_C_Sharp_LINQ/Root.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Parsing_C_Sharp_LINQ
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public class root
+    {
+
+        private rootElement[] elementField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("element")]
+        public rootElement[] element
+        {
+            get
+            {
+                return this.elementField;
+            }
+            set
+            {
+                this.elementField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public class rootElement
+    {
+
+        private rootElementElement[] wEEKField;
+
+        private string wHOField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("element", IsNullable = false)]
+        public rootElementElement[] WEEK
+        {
+            get
+            {
+                return this.wEEKField;
+            }
+            set
+            {
+                this.wEEKField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string WHO
+        {
+            get
+            {
+                return this.wHOField;
+            }
+            set
+            {
+                this.wHOField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public class rootElementElement
+    {
+
+        private rootElementElementElement[] eXPENSEField;
+
+        private byte nUMBERField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("element", IsNullable = false)]
+        public rootElementElementElement[] EXPENSE
+        {
+            get
+            {
+                return this.eXPENSEField;
+            }
+            set
+            {
+                this.eXPENSEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte NUMBER
+        {
+            get
+            {
+                return this.nUMBERField;
+            }
+            set
+            {
+                this.nUMBERField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public class rootElementElementElement
+    {
+
+        private decimal aMOUNTField;
+
+        private string wHATField;
+
+        /// <remarks/>
+        public decimal AMOUNT
+        {
+            get
+            {
+                return this.aMOUNTField;
+            }
+            set
+            {
+                this.aMOUNTField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string WHAT
+        {
+            get
+            {
+                return this.wHATField;
+            }
+            set
+            {
+                this.wHATField = value;
+            }
+        }
+    }
+
+
+}
